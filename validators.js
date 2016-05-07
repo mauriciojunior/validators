70a8d2ebf3c91acac9f00af1cc443cc35667d856
Maurício Júnior <mjuniior92@gmail.com>
2016-05-06 23:24:13 -0300

    Update validators.js

--- validators.js
+++ validators.js
@@ -2,65 +2,65 @@ var validators = (function() {
 
   this.validCPF = function( cpf ) {
 
-    var cpf = clearCPF();
+    var cpf = clearCPF()
 
-    if( errorLength() ) {
-      return false;
+    if(errorLength()) {
+      return false
     }
-    var digitOne = +getDigits()[0];
-    var digitTwo = +getDigits()[1];
+    var digitOne = +getDigits()[0]
+    var digitTwo = +getDigits()[1]
 
-    var firstDigitChecked = checkerOneDigit();
-    var secondDigitChecked = checkerTwoDigit();
+    var firstDigitChecked = checkerOneDigit()
+    var secondDigitChecked = checkerTwoDigit()
 
-    function clearCPF() {
-      return cpf.replace( /\D/g, '' );
-    }
-
-    function getDigits() {
-      return cpf.slice( 9 );
+    function clearCPF () {
+      return cpf.replace( /\D/g, '' )
     }
 
-    function checkerOneDigit() {
-      var aux = 0;
-      for( var i = 10, x = 0; i >= 2, x <= 8 ; i--, x++ ) {
-        aux = aux + Number( cpf[ x ] * i );
+    function getDigits () {
+      return cpf.slice(9)
+    }
+
+    function checkerOneDigit () {
+      var aux = 0
+      for ( var i = 10, x = 0; i >= 2, x <= 8 ; i--, x++ ) {
+        aux = aux + Number(cpf[x] * i)
       }
-      var digit = ( aux * 10 ) % 11;
+      var digit = (aux * 10) % 11
       if ( digit === 10 || digit === 11 ) {
-        return 0;
+        return 0
       }
 
-      return digit;
+      return digit
     }
 
     function checkerTwoDigit() {
-      var aux = 0;
-      for( var i = 11, x = 0; i >= 2, x <= 9 ; i--, x++ ) {
-        aux = aux + Number( cpf[ x ] * i );
+      var aux = 0
+      for ( var i = 11, x = 0; i >= 2, x <= 9 ; i--, x++ ) {
+        aux = aux + Number( cpf[ x ] * i )
       }
-      var digit = ( aux * 10 ) % 11;
+      var digit = ( aux * 10 ) % 11
       if ( digit === 10 || digit === 11 ) {
-        return 0;
+        return 0
       }
 
-      return digit;
+      return digit
     }
     function errorLength() {
-      return cpf.length !== 11;
+      return cpf.length !== 11
     }
 
     function isValid() {
       if( firstDigitChecked === digitOne && secondDigitChecked === digitTwo ) {
-        return true;
+        return true
       }
-      return false;
+      return false
     }
 
-    return isValid();
-  };
+    return isValid()
+  }
 
   return {
     validCPF: validCPF
-  };
-})();
+  }
+})()
